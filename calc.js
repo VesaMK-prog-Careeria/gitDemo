d93921b03e908b7b94995f9b1ff8c983e76aa924
Toni Pekkala <toni.pekkala@hotmail.com>
2024-01-30 20:19:27 +0200

    toinen muokkaus

--- calc.js
+++ calc.js
@@ -24,7 +24,7 @@ function divide(a, b) {
 
 // Usage
 console.log(add(5, 3)); // Output: 10
-console.log(subtract(10, 4)); // Output: 6
+console.log(subtract(10, 4)); // Output: 8
 console.log(multiply(2, 6)); // Output: 12
 console.log(divide(10, 2)); // Output: 5
 console.log(divide(10, 0)); // Output: Error: Division by zero is not allowed.
